2d50db4918a8755c2fabedab57e3b0055b63493f
Aji Tri Prasetyoo <arvan12aa@gmail.com>
2026-04-24 16:38:52 +0700

    Prepare for Vercel deployment

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB6Z0K0JqpHa9746IOaxToEgW5vGcL7X1w",
-  authDomain: "purbatech-db719.firebaseapp.com",
-  projectId: "purbatech-db719",
-  storageBucket: "purbatech-db719.firebasestorage.app",
-  messagingSenderId: "660872569420",
-  appId: "1:660872569420:web:e1e2e6d1d8f0e450578ae9",
-  measurementId: "G-5XDTEK12F6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
